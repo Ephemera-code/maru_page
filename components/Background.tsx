d225668d97b688f4ce848530a671df107f709f35
Ephemera-code <Psncards789@gmail.com>
2026-02-17 16:29:47 -0300

    Fix background jumps on mobile scroll with 10% height buffer and green fallback color

--- components/Background.tsx
+++ components/Background.tsx
@@ -3,14 +3,17 @@ import Image from 'next/image';
 
 export default function Background() {
     return (
-        <div className="fixed top-0 left-0 w-full h-lvh -z-50 overflow-hidden pointer-events-none">
+        <div
+            className="fixed top-[-5lvh] left-0 w-full h-[110lvh] -z-50 overflow-hidden pointer-events-none transform-gpu will-change-transform"
+            style={{ backfaceVisibility: 'hidden' }}
+        >
             <Image
                 alt="Background"
                 src="/images/fondo_page3.svg"
                 fill
                 className="object-cover object-center opacity-100 w-full h-full"
                 priority
-                sizes="100vw"
+                sizes="110vw"
             />
             {/* Optional overlay if image is too bright/dark */}
             {/* <div className="absolute inset-0 bg-white/30 backdrop-blur-[2px]" /> */}
